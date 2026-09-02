f071ad4fa998b00e42a936e451a94f6a75fc6a2b
ahmad-alaa-ayoub <ahmad.alaa.ayoub@gmail.com>
2026-09-02 16:28:06 +0300

    v2.4

--- src/main.tsx
+++ src/main.tsx
@@ -3,14 +3,12 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import App from './app'
 
-
 // @ts-ignore
 import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    { }
-    <BrowserRouter>
+    <BrowserRouter basename={import.meta.env.BASE_URL}>
       <App />
     </BrowserRouter>
   </React.StrictMode>,
